525e7bb33a222a78a02226fc006a49c35a5d5063
Fabian Rivera <frakxman@hotmail.com>
2020-11-27 20:36:58 -0500

    Proyect refactory in sapper

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,12 @@
+<!-- layout.js  -->
+<script>
+    import Header from '../components/Header.svelte';
+    export let segment;
+</script>
+
+<!-- layout.html  -->
+<Header { segment } />
+
+<main>
+    <slot />
+</main>
